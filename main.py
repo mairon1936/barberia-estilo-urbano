fea5ace261e15a09ac9a4db1155f0e8d10437d6c
mairon <ragelitogavina@gmail.com>
2026-09-24 16:03:52 -0400

    agregamos input

--- main.py
+++ main.py
@@ -49,3 +49,14 @@ def generar_recibo(nombre_negocio, nombre_cliente, mensaje_Bienvenida):
 
 
 print(generar_recibo(nombre_negocio, "Carlos", generar_bienvenida(nombre_negocio, precio_corte, cliente_frecuente, primera_visita)))
+
+print(generar_bienvenida("Estilo	Urbano",	350,	True,	True))
+
+
+nombre_cliente	=	input("¿Cuál	es	tu	nombre?	")
+respuesta_frecuente	=	input("¿Eres	cliente	frecuente?	(si/no)	")
+respuesta_visita	=	input("¿Es	tu	primera	visita	este	mes?	(si/no)	")
+es_frecuente	=	respuesta_frecuente.lower()	==	"si"
+es_primera_visita	=	respuesta_visita.lower()	==	"si"
+mensaje	=	generar_bienvenida("Estilo	Urbano",	350,	es_frecuente,	es_primera_visita)
+print(generar_recibo(nombre_negocio, nombre_cliente, mensaje))
